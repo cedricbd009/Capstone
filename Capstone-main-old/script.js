4c978dc46428f059f49b66aa1bd7b72661977a31
Astra-Tau <77858721+Astra-Tau@users.noreply.github.com>
2023-10-09 10:30:41 -0400

    Add files via upload

---
+++ Capstone-main-old/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    const cartItems = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+    let total = 0;
+
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const product = button.parentElement;
+            const productName = product.querySelector('h2').textContent;
+            const productPrice = parseFloat(product.querySelector('p').textContent.replace('$', ''));
+            
+            const cartItem = document.createElement('li');
+            cartItem.textContent = `${productName} - $${productPrice}`;
+            cartItems.appendChild(cartItem);
+            
+            total += productPrice;
+            cartTotal.textContent = `$${total.toFixed(2)}`;
+        });
+    });
+});
