fa16bd17052ee3956037df7c6cbae1f2e1eddb01
Astra-Tau <77858721+Astra-Tau@users.noreply.github.com>
2023-11-08 05:44:50 -0500

    Add files via upload

--- scripts/curriculum.js
+++ scripts/curriculum.js
@@ -101,6 +101,18 @@ function check_key_curr(event)
 
 
 
+function reset_curr_filters()
+{
+    if (search_bar != null)
+    {
+        search_bar.value = "";
+    }
+
+    filter_curr_page();
+}
+
+
+
 // This only works if this file is loaded before the data_getter file.
 // MAKE SURE that this file is listed ABOVE the data_getter file in the script block.
 // The data_getter file has to have the SAME or lower load priority than this file. If this file is DEFER, data_getter MUST be DEFER.
